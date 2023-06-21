ef84806cc3b7f9984d030f941e9e8d7913a2f6cc
emiryumak <emiryumak@gmail.com>
2023-06-21 14:55:14 +0300

    UUID Changed

--- projects/common/utility/uuid.ts
+++ projects/common/utility/uuid.ts
@@ -6,10 +6,8 @@
  * found in the LICENSE file at https://rilke.ist/license
  */
 
+import { v4 as uuidv4 } from 'uuid';
+
 export function uuid() {
-	const dateStr = Date.now().toString(36); // convert num to base 36 and stringify
-
-	const randomStr = Math.random().toString(36).substring(2, 8); // start at index 2 to skip decimal point
-
-	return `${dateStr}-${randomStr}`;
+	return uuidv4();
 }
